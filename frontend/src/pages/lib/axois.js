0b1890f96a8decf203c7169993969f6156d6f9c3
Mekha Dharshini <mekharamesh3@gmail.com>
2026-05-18 11:41:18 +0530

    fixed 404

--- frontend/src/pages/lib/axois.js
+++ frontend/src/pages/lib/axois.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api", // your backend URL
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:5001/api"
+    : "/api",
+
   withCredentials: true,
 });
 
-export const getAuthUser = async ()=>{
+export const getAuthUser = async () => {
   const res = await axiosInstance.get("/auth/me");
   return res.data;
-}
+};
